db137dab48f76e0a68b36363dd09bcf106effde8
Divyanshu Patel <divyanshupatel15243@gmail.com>
2026-05-25 17:43:23 +0530

    landing updaedfht

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { LanguageProvider } from "@/i18n/LanguageContext";
-import Landing from "@/components/Landing";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
 import "./styles.css";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <Landing />
-    </LanguageProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
